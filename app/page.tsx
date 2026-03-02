622bdeb91572449d46f54b4b4a8305296158cb78
CodeMonkey <abhimanyug987@gmail.com>
2026-03-02 08:05:58 +0530

    added dashboard layout & sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
-import { Button } from "@/components/ui/button";
-import Logout from "@/module/auth/components/logout";
 import { requireAuth } from "@/module/auth/utils/auth-utils";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   await requireAuth();
 
-  return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <Logout>
-        <Button className="cursor-pointer">Hello World</Button>
-      </Logout>
-    </div>
-  );
+  return redirect("/dashboard");
 }
